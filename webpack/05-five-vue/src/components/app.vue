4078ca2b8ec7409809717bf5ca001f4e53b89098
guowenfh <guowen921@live.com>
2016-03-21 12:32:46 +0800

    完成对vue文件的加载，并且配置自动刷新，sass编译

---
+++ webpack/05-five-vue/src/components/app.vue
@@ -0,0 +1,33 @@
+<script>
+    export default {
+        el:"#app",
+        data () {
+            return {
+                name:"guowenfh",
+                age:"21"
+            }
+        }
+    }
+</script>
+
+<template>
+<div style="background-color:rgba(0,0,0,0.2)">
+    <h1>姓名：{{name}}</h1>
+    <h2>{{age}}</h2>
+摄动方法
+</div>
+</template>
+
+<style lang="sass">
+    $qwe:red;
+    body{
+        background-color: $qwe;
+    }
+    h1{
+        background-color: #eee;
+        color: red;
+    }
+    h2{
+        background-color: #999;
+    }
+</style>
