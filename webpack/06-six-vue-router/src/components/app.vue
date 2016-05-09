eb45a64b23005e41a3bd62a4be530e62084991d1
guowenfh <guowen921@live.com>
2016-05-09 22:47:32 +0800

    回滚06

--- webpack/06-six-vue-router/src/components/app.vue
+++ webpack/06-six-vue-router/src/components/app.vue
@@ -1,24 +1,55 @@
-<template>
-  <router-view></router-view>
-</template>
-
 <script>
+    import hello from "./hello.vue"
+    export default {
+        data () {
+            return {
+                name:"guowenfh",
+                age:"21"
+            }
+        },
 
-  export default {
-    // replace: false,
-    data() {
-      return {
-      }
-    },
-    methods: {
+        methods :{
+            golist (){
+                this.$route.router.go({name:"list"});
+            }
+        },
+        components:{
+            hello
+        }
 
-    },
-    ready() {
-
-    },
-    components: {
 
     }
-  }
-
 </script>
+
+<template>
+<div>
+    <h1>姓名：{{name}}</h1>
+    <h2>{{age}}</h2>
+    <button @click="golist">$route.router.go查看</button>
+    <a v-link="{ name: 'list' }">v-link查看列表</a>
+    <a v-link="{ name: 'index' }">回去主页</a>
+    <a v-link="{ name: 'hello' }">嵌套的路由</a>
+    <hello></hello>
+    <router-view></router-view>
+
+</div>
+</template>
+
+<style lang="sass">
+    $qwe:pink;
+    body{
+        background-color: $qwe;
+        color:red;
+    }
+    h1{
+        background-color: blue;
+        color: red;
+    }
+    h2{
+        background-color:purple ;
+    }
+    a{
+        font-size:34px;
+        color:black;
+    }
+</style>
